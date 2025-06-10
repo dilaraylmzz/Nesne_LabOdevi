15ecc3750e13ff68e343816f804b26426f4f987e
dilaraylmzz <139263243+dilaraylmzz@users.noreply.github.com>
2025-06-10 20:31:15 +0300

    İlk commit

---
+++ deney1/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace deney1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOrtalamaHesapla = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnDonemOrt = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(368, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Vize Notu:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(280, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Final Notu:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnOrtalamaHesapla
+            // 
+            this.btnOrtalamaHesapla.BackColor = System.Drawing.Color.Bisque;
+            this.btnOrtalamaHesapla.Location = new System.Drawing.Point(41, 157);
+            this.btnOrtalamaHesapla.Name = "btnOrtalamaHesapla";
+            this.btnOrtalamaHesapla.Size = new System.Drawing.Size(139, 38);
+            this.btnOrtalamaHesapla.TabIndex = 4;
+            this.btnOrtalamaHesapla.Text = "Ortalama Hesapla";
+            this.btnOrtalamaHesapla.UseVisualStyleBackColor = false;
+            this.btnOrtalamaHesapla.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.Bisque;
+            this.btnEkle.Location = new System.Drawing.Point(260, 157);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(139, 38);
+            this.btnEkle.TabIndex = 5;
+            this.btnEkle.Text = "Diziye Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnDonemOrt
+            // 
+            this.btnDonemOrt.BackColor = System.Drawing.Color.Bisque;
+            this.btnDonemOrt.Location = new System.Drawing.Point(487, 161);
+            this.btnDonemOrt.Name = "btnDonemOrt";
+            this.btnDonemOrt.Size = new System.Drawing.Size(256, 34);
+            this.btnDonemOrt.TabIndex = 6;
+            this.btnDonemOrt.Text = "Dönem Ortalaması Hesapla";
+            this.btnDonemOrt.UseVisualStyleBackColor = false;
+            this.btnDonemOrt.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ortalama sonucu burada";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(257, 273);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Geçti/Kaldı bilgisi";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(435, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Harf notu";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(557, 273);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(199, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Ekleme durumu veya dönem ort.";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(786, 344);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnDonemOrt);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.btnOrtalamaHesapla);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOrtalamaHesapla;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnDonemOrt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ deney1/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace deney1
+{
+    public partial class Form1 : Form
+    {
+        const int GECMENOTU = 50;
+        const int DERSSAYISI = 5;
+
+        double[] dersnotlari = new double[DERSSAYISI];
+        int yertutucu = 0;
+        double dersnotu = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox1.Text, out int vize) ||
+                 !int.TryParse(textBox2.Text, out int finaln))
+            {
+                MessageBox.Show("Lütfen geçerli notlar girin.");
+                return;
+            }
+
+            int ortalama = (int)(vize * 0.4 + finaln * 0.6);
+            label3.Text = ortalama.ToString();
+
+            if (ortalama >= GECMENOTU)
+                label4.Text = "Geçti";
+            else
+                label4.Text = "Kaldı";
+
+            if (ortalama < GECMENOTU)
+                dersnotu = 0;
+            else if (ortalama < 60)
+                dersnotu = 1;
+            else if (ortalama < 70)
+                dersnotu = 2;
+            else if (ortalama < 85)
+                dersnotu = 3;
+            else
+                dersnotu = 4;
+
+            label5.Text = dersnotu.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (yertutucu >= DERSSAYISI)
+            {
+                label6.Text = "Dizi doldu";
+                return;
+            }
+
+            dersnotlari[yertutucu++] = dersnotu;
+            label6.Text = "Eklendi";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double toplam = 0;
+            int gecenDersSayisi = 0;
+
+            for (int i = 0; i < DERSSAYISI; i++)
+            {
+                if (dersnotlari[i] < 2.5)
+                    continue;
+
+                toplam += dersnotlari[i];
+                gecenDersSayisi++;
+            }
+
+            if (gecenDersSayisi == 0)
+            {
+                label6.Text = "Geçen ders yok";
+                return;
+            }
+
+            double donemOrt = toplam / gecenDersSayisi;
+            label6.Text = "Dönem Ort: " + donemOrt.ToString("0.00");
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
