552f4c4c41debf731b5564c1ea740e33e4b5901a
dilaraylmzz <139263243+dilaraylmzz@users.noreply.github.com>
2025-06-10 20:34:36 +0300

    İlk commit

---
+++ deney3/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace deney3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSonuc1 = new System.Windows.Forms.Label();
+            this.lblSonuc2 = new System.Windows.Forms.Label();
+            this.lblSonuc3 = new System.Windows.Forms.Label();
+            this.btnTestDortgenler = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSonuc1
+            // 
+            this.lblSonuc1.AutoSize = true;
+            this.lblSonuc1.Location = new System.Drawing.Point(110, 172);
+            this.lblSonuc1.Name = "lblSonuc1";
+            this.lblSonuc1.Size = new System.Drawing.Size(44, 16);
+            this.lblSonuc1.TabIndex = 0;
+            this.lblSonuc1.Text = "label1";
+            // 
+            // lblSonuc2
+            // 
+            this.lblSonuc2.AutoSize = true;
+            this.lblSonuc2.Location = new System.Drawing.Point(237, 172);
+            this.lblSonuc2.Name = "lblSonuc2";
+            this.lblSonuc2.Size = new System.Drawing.Size(44, 16);
+            this.lblSonuc2.TabIndex = 1;
+            this.lblSonuc2.Text = "label2";
+            // 
+            // lblSonuc3
+            // 
+            this.lblSonuc3.AutoSize = true;
+            this.lblSonuc3.Location = new System.Drawing.Point(362, 172);
+            this.lblSonuc3.Name = "lblSonuc3";
+            this.lblSonuc3.Size = new System.Drawing.Size(44, 16);
+            this.lblSonuc3.TabIndex = 2;
+            this.lblSonuc3.Text = "label3";
+            // 
+            // btnTestDortgenler
+            // 
+            this.btnTestDortgenler.Location = new System.Drawing.Point(133, 83);
+            this.btnTestDortgenler.Name = "btnTestDortgenler";
+            this.btnTestDortgenler.Size = new System.Drawing.Size(246, 49);
+            this.btnTestDortgenler.TabIndex = 3;
+            this.btnTestDortgenler.Text = "Dortgenleri Oluştur ve Test Et";
+            this.btnTestDortgenler.UseVisualStyleBackColor = true;
+            this.btnTestDortgenler.Click += new System.EventHandler(this.btnTestDortgenler_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTestDortgenler);
+            this.Controls.Add(this.lblSonuc3);
+            this.Controls.Add(this.lblSonuc2);
+            this.Controls.Add(this.lblSonuc1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSonuc1;
+        private System.Windows.Forms.Label lblSonuc2;
+        private System.Windows.Forms.Label lblSonuc3;
+        private System.Windows.Forms.Button btnTestDortgenler;
+    }
+}
+
---
+++ deney3/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace deney3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTestDortgenler_Click(object sender, EventArgs e)
+        {
+            // 1. Varsayılan kurucu ile Dortgen oluşturma
+            Dortgen d1 = new Dortgen();
+            lblSonuc1.Text = $"d1 (Varsayılan): x={d1.x}, y={d1.y}, Alan={d1.Alan()}";
+
+            // 2. Parametreli kurucu ile Dortgen oluşturma
+            Dortgen d2 = new Dortgen(5, 7);
+            lblSonuc2.Text = $"d2 (Parametreli): x={d2.x}, y={d2.y}, Alan={d2.Alan()}";
+
+            // 3. Kopyalama kurucu ile Dortgen oluşturma ve DegerVer metodunu kullanma
+            Dortgen d3 = new Dortgen(d2); // d2'nin kopyasını oluşturur
+            d3.DegerVer(10, 4); // d3'ün değerlerini değiştirir
+            lblSonuc3.Text = $"d3 (Kopyalanmış ve Değer Verilmiş): x={d3.x}, y={d3.y}, Alan={d3.Alan()}";
+
+            // 4. Buyuk metodunu kullanarak büyük olanı bulma
+            Dortgen enBuyukDortgen = d1.Buyuk(d2);
+            MessageBox.Show($"d1 ve d2'den büyük olanın alanı: {enBuyukDortgen.Alan()}", "En Büyük Dikdörtgen");
+
+            // d3 ve d2 arasındaki farkı göstermek için d2'nin alanını tekrar gösterelim
+            // (d3'ü değiştirmek d2'yi etkilemez)
+            MessageBox.Show($"d2'nin alanı hala: {d2.Alan()}", "d2 Alanı Kontrol");
+        }
+    }
+}
